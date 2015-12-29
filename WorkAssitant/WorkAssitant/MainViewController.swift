247a4c045f192578674ad7f0f2dc80a449219894
youngtry <youngtry@163.com>
2015-12-30 00:18:05 +0800

    code

---
+++ WorkAssitant/WorkAssitant/MainViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewController.swift
+//  WorkAssitant
+//
+//  Created by  TianRunyan on 15/12/29.
+//  Copyright © 2015年  TianRunyan. All rights reserved.
+//
+
+import Foundation
---
+++ WorkAssitant/WorkAssitant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkAssitant
+//
+//  Created by  TianRunyan on 15/12/29.
+//  Copyright © 2015年  TianRunyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
